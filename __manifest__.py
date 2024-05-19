f199620e2098cfba33f285d9fbce0df80e895d3b
unknown <mostafamahmoud7598@gamil.com>
2024-05-19 20:24:38 +0200

    [init]

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate Management",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "Quadova",
+    'website': "https://quadova.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'Estate_Management/static/src/js/video_display_widget.js',
+
+    #     ],
+    # }
+}
