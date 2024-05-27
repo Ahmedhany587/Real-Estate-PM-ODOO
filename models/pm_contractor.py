dfdb2724300dfc7c77f73436e5d66dcb5a327a13
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2024-05-27 10:50:58 +0300

    [Feature] Add the billing functionality to the contract view

--- models/pm_contractor.py
+++ models/pm_contractor.py
@@ -1,7 +1,39 @@
-from odoo import api, fields, models
+from datetime import timedelta
+from odoo import _, fields, models, Command
 
 class Contractor(models.Model):
     _inherit = 'res.partner'
 
     contractor_subterm_ids = fields.One2many(comodel_name='pm.contractor.subterm', inverse_name='contractor_id', string="Contractor Sub-Term", ondelete='restrict')
     
+    ### views ###
+    def make_bill(self):
+        
+        invoice_line_ids =  []
+
+        for subterm in self.contractor_subterm_ids:
+            if subterm.state not in ('finished', 'cancelled'):
+                continue
+            invoice_line_ids.append(
+                Command.create({'product_id': subterm.service_id.id, 'quantity': subterm.progress})
+            )
+
+        new_bill = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'partner_id': self.id,
+            'invoice_date': fields.Date.today(),
+            'invoice_date_due': fields.Date.today() + timedelta(days=30),
+            'invoice_line_ids': invoice_line_ids,
+        })
+
+        action = {
+            'name': _("Vendor Bills"),
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.move',
+            'views': [(self.env.ref("account.view_move_form").id, 'form')],
+            'target': 'new',
+            'context': {'default_move_type': 'in_invoice'},
+            'res_id': new_bill.id
+        }
+
+        return action
