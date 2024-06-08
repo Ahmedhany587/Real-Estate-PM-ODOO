095dcbac3c9bd630f015e57459612324a26cac3f
Ahmed hany <ahmedhany216216@gmail.com.com>
2024-06-08 15:12:54 +0300

    [Add] assigned_qty must be <= needed_qty validation

--- models/pm_product.py
+++ models/pm_product.py
@@ -1,16 +1,21 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class NeededProduct(models.Model):
     """
     Inherit product.template model to add needed_qty and assigned_qty fields.
     """
-    _name = 'product.template'
     _inherit = 'product.template'
 
     needed_qty = fields.Integer(
-        string='Needed Quantity', default=0, 
+        string='Needed Quantity', default=0,
         help="The quantity of this product that is needed.")
     assigned_qty = fields.Integer(
-        string='Assigned Quantity', default=0, 
+        string='Assigned Quantity', default=0,
         help="The quantity of this product that has been assigned to a contractor.")
-    
+
+    @api.constrains('assigned_qty', 'needed_qty')
+    def _check_assigned_qty(self):
+        for record in self:
+            if record.assigned_qty > record.needed_qty:
+                raise ValidationError("Assigned quantity cannot exceed needed quantity.")
