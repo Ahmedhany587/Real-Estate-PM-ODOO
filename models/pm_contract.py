5d73d4455529a0e795408889600ae72210f1578b
Ahmed hany <ahmedhany216216@gmail.com.com>
2024-06-09 13:00:50 +0300

    [Add] States for Contract

--- models/pm_contract.py
+++ models/pm_contract.py
@@ -1,44 +1,72 @@
 from odoo import api, fields, models
 
-
 class Contract(models.Model):
     """
     Model that represents a real estate contract with a customer.
     """
     _name = 'pm.contract'
     _description = 'Real Estate Contract with the Customer'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    # Name of the contract
-    name = fields.Char()
-
-    # Customer associated with the contract
+    name = fields.Char(string="Contract Name", tracking=True)
     partner_id = fields.Many2one(
         comodel_name='res.partner',
-        string="Customer",  
-        required=True,  
-        index=True,  
+        string="Customer",
+        required=True,
+        index=True,
+        tracking=True
     )
-
-    # Date of the contract
     contract_date = fields.Date(
-        string="Contract Date:",  
-        required=False,  
-        default=lambda self: fields.Date.today()  # Default value is today's date
+        string="Contract Date",
+        required=False,
+        default=lambda self: fields.Date.today(),
+        tracking=True
     )
-
-    # Project associated with the contract
     project_id = fields.Many2one(
         comodel_name='pm.project',
-        string="Project",  
-        ondelete='restrict'  
+        string="Project",
+        ondelete='restrict',
+        tracking=True
     )
-
-    # Documents associated with the contract
     doc_ids = fields.One2many(
-        'pm.document',  
+        'pm.document',
         'contract_id',  # Field in the pm.document model that references this contract
-        string='Documents'  
+        string='Documents',
+        tracking=True
     )
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirmed', 'Confirmed'),
+        ('signed', 'Signed'),
+        ('cancel', 'Cancelled'),
+    ], string='Status', readonly=True, default='draft', tracking=True)
+
+    @api.model
+    def create(self, vals):
+        vals['state'] = 'draft'
+        return super(Contract, self).create(vals)
+
+    def action_confirm(self):
+        self.state = 'confirmed'
+
+    def action_sign(self):
+        self.state = 'signed'
+
+    def action_cancel(self):
+        self.state = 'cancel'
+
+    def action_set_to_draft(self):
+        self.state = 'draft'
+
+    @api.onchange('state')
+    def _onchange_state(self):
+        if self.state == 'signed':
+            for field in self._fields:
+                if field != 'doc_ids':
+                    self._fields[field].readonly = True
+        else:
+            for field in self._fields:
+                self._fields[field].readonly = False
 
 class Document(models.Model):
     """
@@ -48,11 +76,9 @@ class Document(models.Model):
     _description = 'PM Document'
 
     name = fields.Char(string='Name')
-
-    #: The actual binary content of the document.
     document = fields.Binary(string='Document')
-
-    #: The property that the document belongs to.
     contract_id = fields.Many2one(
-        'pm.contract', string='Contract',
-        help='The contract that this document is associated with.')
+        'pm.contract',
+        string='Contract',
+        help='The contract that this document is associated with.'
+    )
